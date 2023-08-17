8b7d32096a9e9d091eae15805cd7801d43318e39
Giovanna Oliveira <107190603+ansiee@users.noreply.github.com>
2023-08-17 20:04:33 -0300

    Add files via upload

---
+++ Python/exercicio-1.py
@@ -0,0 +1,7 @@
+# Faça um programa que converta metros em centímetros.
+
+numero = float(input("Escreva o número em metros (Sem o uso do m):"))
+
+conversao = numero * 100
+
+print(f"O seu número convertido é {conversao}cm.")
